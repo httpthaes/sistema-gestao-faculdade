6e65f4c88e6a7056734c08eaccade532d517e3cc
RenataAires <renataaires8332@gmail.com>
2026-08-29 18:50:44 -0300

    feat: cria modelo de Curso e enum TipoCurso.

---
+++ Models/Curso.cs
@@ -0,0 +1,8 @@
+namespace SistemaGestaoFaculdade.Models;
+
+public class Curso
+{
+    public string Codigo {get; private set;}
+    public string Nome {get; private set;}
+    public TipoCurso Tipo {get; private set;}
+}
---
+++ Models/TipoCurso.cs
@@ -0,0 +1,7 @@
+namespace SistemaGestaoFaculdade.Models;
+
+public enum TipoCurso
+{
+    Graduacao = 1,
+    PosGraduacao = 2
+}
